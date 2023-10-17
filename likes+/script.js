aedf081f5f0b03e465c603c64dcc99928a399ea7
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2023-10-17 01:12:26 +0100

    Likes_Assignment_Part2

---
+++ likes+/script.js
@@ -0,0 +1,23 @@
+//function of first button
+var nbr1=document.querySelector("#nbrlike1");
+var cont=9;
+function nbreOfLike1(){
+    cont++;
+    nbr1.innerText= cont+" "+"like(s)";
+}
+
+//function of second button
+var nbr2=document.querySelector("#nbrlike2");
+var count=12;
+function nbreOfLike2(){
+    count++;
+    nbr2.innerText= count+" "+"like(s)";
+}
+
+//function of third button
+var nbr3=document.querySelector("#nbrlike3");
+var counter=9;
+function nbreOfLike3(){
+    counter++;
+    nbr3.innerText= counter+" "+"like(s)";
+}
