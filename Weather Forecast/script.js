becf8e9a8e8024ba33ecbf0528068712757cb0eb
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2023-10-18 03:20:49 +0100

    Dojo Weather

---
+++ Weather Forecast/script.js
@@ -0,0 +1,59 @@
+//remove function
+var buttomOfPage=document.querySelector(".footer");
+function hide(element){
+    buttomOfPage.remove();
+}
+
+//alert function
+function msg(element){
+    alert('Loading weather report...');
+}
+
+
+
+
+
+
+var val=[ document.querySelector("#firstsp1")
+,document.querySelector("#firstsp3")
+,document.querySelector("#firstsp5")
+,document.querySelector("#firstsp7")
+,document.querySelector("#secondsp2")
+,document.querySelector("#secondsp4")
+,document.querySelector("#secondsp6")
+,document.querySelector("#secondsp8")
+]
+
+function changeOn(element){
+    val.innertext["75","80","69","78","65","66","61","70"];
+}
+
+
+
+
+
+
+
+
+
+/*var firstF1=document.querySelector("#firstsp1");
+var firstF3=document.querySelector("#firstsp3");
+var firstF5=document.querySelector("#firstsp5");
+var firstF7=document.querySelector("#firstsp7");
+
+var secondF2=document.querySelector("#secondsp2");
+var secondF4=document.querySelector("#secondsp4");
+var secondF6=document.querySelector("#secondsp6");
+var secondF8=document.querySelector("#secondsp8");
+
+function changeOn(element){
+    firstF1.inneText="75°";
+    firstF3.inneText="80°";
+    firstF5.inneText="69°";
+    firstF7.inneText="78°";
+
+    secondF2.innerText="65°";
+    secondF4.innerText="65°";
+    secondF6.innerText="65°";
+    secondF8.innerText="65°";
+}*/
